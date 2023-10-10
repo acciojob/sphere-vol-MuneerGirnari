cf47405dfb0c2f0ca8198f0e652d596a33fdfe23
Muneer Girnari <muneeragirnari@gmail.com>
2023-10-10 20:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
+let form = document.getElementById("MyForm");
+
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+    let rad = form.radius.value;
+	form.volume.value =  (4*Math.PI*rad*rad*rad)/3;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
